b0b845744971dc9f9a82ab502f3af3f00ef68edd
Kyle Shepherd <kyle@thisissoon.com>
2025-06-27 16:20:16 +0100

    test(test.tsx): Fix tests

--- test.tsx
+++ test.tsx
@@ -1,48 +1,63 @@
-import { render, screen} from '@testing-library/react';
-import Home from '@/app/page';
+import { render, screen } from "@testing-library/react";
+import Home from "@/app/page";
+import { act } from "react-dom/test-utils";
 
-describe('Home', () => {
-    it('renders an empty basket', () => {
-        render(<Home />);
-
-        const basketButton = screen.getByRole('button', {
-            name: /Basket:/i,
-        });
-
-        expect(basketButton).toHaveTextContent('Basket: 0 items');
+describe("Home", () => {
+  it("renders an empty basket", () => {
+    act(() => {
+      render(<Home />);
     });
 
-    it('renders a basket with 1 item', async () => {
-        render(<Home />);
-
-        const buttons = screen.getAllByRole('button', {
-            name: /Add to basket/i,
-        });
-
-        await buttons[0].click();
-
-        const basketButton = screen.getByRole('button', {
-            name: /Basket:/i,
-        });
-
-        expect(basketButton).toHaveTextContent(/Basket: 1 item$/);
+    const basketButton = screen.getByRole("button", {
+      name: /Basket:/i,
     });
 
-    it('renders a basket with 1 of item 1 and 2 of item 2', async () => {
-        render(<Home />);
+    expect(basketButton).toHaveTextContent("Basket: 0 items");
+  });
 
-        const buttons = screen.getAllByRole('button', {
-            name: /Add to basket/i,
-        });
-
-        await buttons[0].click();
-        await buttons[1].click();
-        await buttons[1].click();
-
-        const basketButton = screen.getByRole('button', {
-            name: /Basket:/i,
-        });
-
-        expect(basketButton).toHaveTextContent(/Basket: 2 items$/);
+  it("renders a basket with 1 item", async () => {
+    act(() => {
+      render(<Home />);
     });
+
+    const buttons = screen.getAllByRole("button", {
+      name: /Add to basket/i,
+    });
+
+    await act(async () => {
+      await buttons[0].click();
+    });
+
+    const basketButton = screen.getByRole("button", {
+      name: /Basket:/i,
+    });
+
+    expect(basketButton).toHaveTextContent(/Basket: 1 item$/);
+  });
+
+  it("renders a basket with 1 of item 1 and 2 of item 2", async () => {
+    act(() => {
+      render(<Home />);
+    });
+
+    const buttons = screen.getAllByRole("button", {
+      name: /Add to basket/i,
+    });
+
+    await act(async () => {
+      await buttons[0].click();
+    });
+    await act(async () => {
+      await buttons[1].click();
+    });
+    await act(async () => {
+      await buttons[1].click();
+    });
+
+    const basketButton = screen.getByRole("button", {
+      name: /Basket:/i,
+    });
+
+    expect(basketButton).toHaveTextContent(/Basket: 3 items$/);
+  });
 });
